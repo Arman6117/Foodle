b90a2a383c70526b472e4c1a5063adff104007bc
Arman Patel <vanixgaming634@gmail.com>
2023-09-13 14:31:57 +0530

    Working on get request

---
+++ app/api/orders/[id]/route.js
@@ -0,0 +1,14 @@
+import { connectDb } from "@/utils/database";
+import Order from "@/models/order";
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectDb();
+
+    const orders = await Order.find({ userId: params.id });
+
+    return new Response(JSON.stringify(orders), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch orders", { status: 500 });
+  }
+};
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -5,22 +5,52 @@ import { useRouter } from "next/navigation";
 import { useUser, SignOutButton } from "@clerk/nextjs";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import useFoodItemStore from "@/utils/store";
+import { createApi } from "unsplash-js";
 const Profile = () => {
   const router = useRouter();
   const { user } = useUser();
+  const id = user?.id;
   const [orders, setOrders] = useState([]);
+  const api = createApi({
+    accessKey: "vH7g7qaMV-VUwPZfulmtc0UKU7PXSBPPAs3cpXWDkZE",
+  });
+  const [foodItem,setFoodItem] = useState('')
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const response = await fetch(`/api/orders/${id}`);
+      const data = await response.json();
+
+      setOrders(data);
+    };
+
+    if (id) fetchOrders();
+  }, [id]);
 
   useEffect(() => {
-    fetch("api/orders")
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.orders) {
-          setOrders(data.orders);
-        }
-      }).catch((err)=>{
-        console.log(err.message);
-      })
-  },[]);
+    const fetchPhotos = async () => {
+      try {
+        const imgData = await api.search.getPhotos({
+          query: foodItem,
+          orientation: "squarish",
+        });
+        const image = imgData.response.results[0].urls.regular;
+
+        const foodItemDetails = {
+          image,
+          foodItemName: foodItem,
+          price: foodItemPrice,
+        };
+
+        saveToDatabase(foodItemDetails);
+        setData(imgData.response.results[0]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchPhotos();
+  }, [foodItem]);
   return (
     <div>
       <div className="flex bg-lightRed/30 p-1  justify-center sm:justify-normal sm:p-0 sm:h-screen  ">
@@ -33,16 +63,16 @@ const Profile = () => {
               }}
             />
             <div className="mt-14 hidden sm:inline space-y-5 ">
-              {/* <Image
-                src={user.profileImageUrl}
+              <Image
+                src={user?.profileImageUrl}
                 width={130}
                 height={130}
                 alt="profile image"
                 className="object-contain rounded-full sm:h-[180px] sm:w-[180px] cursor-pointer "
                 onClick={() => router.push("/profile")}
-              /> */}
+              />
               <h1 className="ml-10 text-white font-semibold text-xl">
-                {/* {user.fullName} */}
+                {user?.fullName}
               </h1>
             </div>
             <div className="bg-redPrimary sm:bg-white p-1 w-20 flex justify-center rounded-md text-white font-semibold sm:text-black  sm:hover:border-2 sm:hover:border-white sm:hover:bg-redPrimary sm:hover:text-white transition-all hover:text-black sm:mt-14">
@@ -50,11 +80,16 @@ const Profile = () => {
               {/* <button>Sign Out</button> */}
             </div>
           </div>
-          <div className="flex">
-          {orders.map((order)=>(
-
-            <div className="" key={order.key}>{order}</div>
-          ))}
+          <div className="flex space-y-6">
+            {orders.map((order) => (
+              
+              <div className="" key={order._id}>
+                <div>
+                
+                </div>
+                <div></div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
